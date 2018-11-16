c84f4071c4d5ce954e2bd598eff38bb61b736782
Aritra Chakraborty <33465645+AritraChakraborty20@users.noreply.github.com>
2018-11-16 17:26:14 +0530

    updated run option

--- texteditor.py
+++ texteditor.py
@@ -1,5 +1,7 @@
-import sys
+import sys, os
+
 v = sys.version
+
 if "2.7" in v:
 	from Tkinter import *
 	import tkFileDialog
@@ -11,22 +13,22 @@ root = Tk()
 root.title("Text Editor")
 text = Text(root)
 text.grid()
+lsum = Label(root, text = 'Out put:')
+lsum.grid(column=0, sticky=W, pady=4)
+
 
 def saveas():
-	global text
+	global text,save
 	t = text.get("1.0", "end-1c")
 	save = filedialog.asksaveasfilename()
 	file1 = open(save, "w+")
 	file1.write(t)
 	file1.close()
 def run():
-	pass
+	global save
+	code = "python3 "
+	lsum["text"] = "" + str((os.popen(code+save).read()))
 
-
-"""root = Tk()
-root.title("Text Editor")
-text = Text(root)
-text.grid()"""
 button = Button(root, text = "Save", command = saveas)
 run = Button(root, text = "RUN", command = run)
 button.grid()
